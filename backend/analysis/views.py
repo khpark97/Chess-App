cade27305f503124a1ecf1907dd7e2813649ef32
Kihyun Park <khpark970@gmail.com>
2025-04-23 18:55:21 -0700

    Backend done for now

--- backend/analysis/views.py
+++ backend/analysis/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from .services import get_best_moves
 
-# Create your views here.
 def analyze_position(request):
     fen = request.GET.get('fen')
     moves = get_best_moves(fen)
